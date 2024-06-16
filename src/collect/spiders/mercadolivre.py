a9ca21efffadfb066390f7abac751f3d692556a8
Gustavo-B-Morales-S <protected_gustavo_borges@outlook.com>
2024-06-16 10:02:54 -0300

    Feat: Added Extract & Load Process

--- src/collect/spiders/mercadolivre.py
+++ src/collect/spiders/mercadolivre.py
@@ -7,37 +7,37 @@ class MercadolivreSpider(scrapy.Spider):
     start_urls = ["https://lista.mercadolivre.com.br/tenis-corrida-masculino"]
 
     def parse(self, response):
-        current_page = response.css('li.andes-pagination__button.andes-pagination__button--current a::text').get()
+        # Setting page limit
+        current_page_selector = response.css('li.andes-pagination__button.andes-pagination__button--current a::text').get()
         max_page_count = '10'
         
-        product_html_element = 'div.ui-search-result__content'
-        brand_html_element = 'span.ui-search-item__brand-discoverability.ui-search-item__group__element::text'
-        name_html_element = 'h2.ui-search-item__title::text'
-        price_html_element = 'span.andes-money-amount__fraction::text'
-        price_cents_html_element = 'span.andes-money-amount__cents::text'
-        reviews_rating_number_html_element = 'span.ui-search-reviews__rating-number::text'
-        reviews_amount_html_element = 'span.ui-search-reviews__amount::text'
+        # Setting selectors
+        product_selector = 'div.ui-search-result__content'
+        brand_selector = 'span.ui-search-item__brand-discoverability.ui-search-item__group__element::text'
+        name_selector = 'h2.ui-search-item__title::text'
+        price_selector = 'span.andes-money-amount__fraction::text'
+        price_in_cents_selector = 'span.andes-money-amount__cents::text'
+        reviews_rating_number_selector = 'span.ui-search-reviews__rating-number::text'
+        reviews_amount_selector = 'span.ui-search-reviews__amount::text'
         
-        products: List[AnyStr] = response.css(product_html_element)
+        products: List[AnyStr] = response.css(product_selector)
         
         for product in products:
-            print(current_page, '')
-            prices: List[AnyStr] = product.css(price_html_element).getall()
-            cents: List[AnyStr] = product.css(price_cents_html_element).getall()
+            prices: List[AnyStr] = product.css(price_selector).getall()
+            cents: List[AnyStr] = product.css(price_in_cents_selector).getall()
             
              
             yield {
-                'brand': product.css(brand_html_element).get(),
-                'name': product.css(name_html_element).get(),
-                'old_price_reais': prices[0] if len(prices) > 0 else None,
-                'old_price_centavos': cents[0] if len(cents) > 0 else None,
-                'new_price_reais': prices[1] if len(prices) > 1 else None,
-                'new_price_centavos': cents[1] if len(cents) > 1 else None,
-                'reviews_rating_number': response.css(reviews_rating_number_html_element).get(),
-                'reviews_amount': response.css(reviews_amount_html_element).get(),
+                'brand': product.css(brand_selector).get(),
+                'name': product.css(name_selector).get(),
+                'old_price_in_reais': prices[0] if len(prices) > 0 else None,
+                'old_price_in_cents': cents[0] if len(cents) > 0 else None,
+                'new_price_in_reais': prices[1] if len(prices) > 1 else None,
+                'new_price_in_cents': cents[1] if len(cents) > 1 else None,
+                'reviews_rating_number': response.css(reviews_rating_number_selector).get(),
+                'reviews_amount': response.css(reviews_amount_selector).get(),
             } 
-        # Esta é a quantidade de paginas pela qual vou percorrer!
-        if current_page != max_page_count:
-            # Next Page URL
+
+        if current_page_selector != max_page_count:
             next_page_url: str = response.css('li.andes-pagination__button.andes-pagination__button--next a::attr(href)').get()
             yield scrapy.Request(url=next_page_url, callback=self.parse)
---
+++ src/transform/main.py
@@ -0,0 +1,48 @@
+import sqlite3
+import pandas as pd
+from typing import List
+from datetime import datetime
+from pandas import DataFrame
+from sqlite3 import Connection
+
+df: DataFrame = pd.read_json(path_or_buf='../../data/data.jsonl', lines=True)
+
+# Adding metadata columns
+df['_collection_date'] = datetime.now()
+df['_source'] = 'https://lista.mercadolivre.com.br/tenis-corrida-masculino'
+df['reviews_amount'] = df['reviews_amount'].str.replace(pat='[\(\)]', 
+                                                        repl='', 
+                                                        regex=True)
+
+# Setting data types
+numeric_columns_dtypes: dict[str] = {
+    'old_price_in_reais'   : float, 
+    'old_price_in_cents'   : float,
+    'new_price_in_reais'   : float, 
+    'new_price_in_cents'   : float, 
+    'reviews_rating_number': float, 
+    'reviews_amount'       : int
+}
+
+# Getting column labels from dictionary keys
+numeric_columns_labels: List[str] = list(numeric_columns_dtypes)
+
+# Changing data types
+df[numeric_columns_labels] = df[numeric_columns_labels].fillna(value=0) \
+                                                       .astype(dtype=numeric_columns_dtypes)
+
+# Creating new price columns from old/new price columns in reais and cents
+df.insert(loc=2, column='old_price', value=(df['old_price_in_reais'] + df['old_price_in_cents'] / 100))
+df.insert(loc=3, column='new_price', value=(df['new_price_in_reais'] + df['new_price_in_cents'] / 100))
+
+# Removing previous price columns
+df.drop(columns=numeric_columns_labels[0:4], inplace=True)
+                
+                                                
+if __name__ == '__main__':
+    connection: Connection = sqlite3.connect(database='../../data/quotes.db')
+
+    df.to_sql(con=connection, name='Mercado_Livre_Items', if_exists='replace', index=False)
+    
+    connection.close()
+    
